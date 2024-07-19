33afe86234e016bc0f69b2079a03d7bbc8352e77
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-07-19 14:31:36 -0400

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
